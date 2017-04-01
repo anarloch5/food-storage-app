73468a6bede075ba629d677f2ffa5fce2a9008ba
Adrian Narloch <adrian.narloch@outlook.com>
2017-04-01 17:31:19 +0200

    change the architecture

--- src/store/constants.js
+++
@@ -1,8 +0,0 @@
-// Firebase config
-
-export const FirebaseConfig = {
-  apiKey: 'AIzaSyAkBU-KzK-lt6pze_s7Fd14p9DY3wOJ5OU',
-  authDomain: 'food-reserve.firebaseapp.com',
-  databaseURL: 'https://food-reserve.firebaseio.com',
-  storageBucket: 'food-reserve.appspot.com'
-};
